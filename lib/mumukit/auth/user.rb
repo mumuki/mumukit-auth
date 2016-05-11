95b5ebdceb26f041aac8f56d0dd7dd4e18c49959
Agustin Pina <aguspina87@gmail.com>
2016-05-11 10:54:34 -0300

    Refactor user model

--- lib/mumukit/auth/user.rb
+++ lib/mumukit/auth/user.rb
@@ -1,29 +1,31 @@
 require 'auth0'
 
-module Mumukit::Auth::User
+class Mumukit::Auth::User
 
-  def self.update_metadata(social_id, data)
-    client.update_user_metadata social_id, metadata_for(social_id).merge(data)
+  attr_accessor :social_id, :user
+
+  def initialize(social_id)
+    @social_id = social_id
+    @user = client.user @social_id
   end
 
-  def self.metadata_for(social_id)
-    user = get_user social_id
+  def update_metadata(data)
+    client.update_user_metadata social_id, metadata.merge(data)
+  end
+
+  def metadata
     metadata = {}
     apps.each do |app|
-      metadata[app] = user[app] if user[app].present?
+      metadata[app] = @user[app] if @user[app].present?
     end
     metadata
   end
 
-  def self.apps
+  def apps
     ['bibliotheca', 'classroom', 'admin']
   end
 
-  def self.get_user(social_id)
-    client.user social_id
-  end
-
-  def self.client
+  def client
     Auth0Client.new(
         :client_id => ENV['MUMUKI_AUTH0_CLIENT_ID'],
         :client_secret => ENV['MUMUKI_AUTH0_CLIENT_SECRET'],
--- spec/mumukit/user_spec.rb
+++ spec/mumukit/user_spec.rb
@@ -2,14 +2,16 @@ require 'spec_helper'
 
 describe Mumukit::Auth::User do
   describe 'build_metadata' do
-    let(:user) { { bibliotheca: 'foo/bar', classroom: 'foo/baz' }.stringify_keys }
-    let(:auth0) { double('auth0') }
+    let!(:auth0_stub) { double('auth0') }
+    let(:user_data) { { id: 1, bibliotheca: 'foo/bar', classroom: 'foo/baz' }.stringify_keys }
+    let(:auth0) { Mumukit::Auth::User.new('auth0|1') }
 
-    before { expect(Auth0Client).to receive(:new) { auth0 }}
-    before { expect(auth0).to receive(:update_user_metadata).with('auth0|1', user.merge(atheneum: 'test/*')) }
-    before { expect(Mumukit::Auth::User).to receive(:get_user).with('auth0|1').and_return(user) }
+    before { expect(Auth0Client).to receive(:new).and_return(auth0_stub) }
+    before { expect(auth0_stub).to receive(:user).with('auth0|1').and_return(user_data) }
 
-    it { expect { Mumukit::Auth::User.update_metadata('auth0|1', atheneum: 'test/*') }.not_to raise_error }
+    it { expect(auth0.metadata).to eq('bibliotheca' => 'foo/bar', 'classroom' => 'foo/baz') }
+    it { expect(auth0.social_id).to eq('auth0|1') }
+    it { expect(auth0.user).to eq(user_data) }
   end
 
 end
