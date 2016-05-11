74bc21c35c856d5af7d1c8d00595a94f792cdab3
Agustin Pina <aguspina87@gmail.com>
2016-05-11 16:18:26 -0300

    Add tests

--- spec/mumukit/user_spec.rb
+++ spec/mumukit/user_spec.rb
@@ -3,15 +3,34 @@ require 'spec_helper'
 describe Mumukit::Auth::User do
   describe 'build_metadata' do
     let!(:auth0_stub) { double('auth0') }
-    let(:user_data) { { id: 1, bibliotheca: 'foo/bar', classroom: 'foo/baz' }.stringify_keys }
     let(:auth0) { Mumukit::Auth::User.new('auth0|1') }
-
     before { expect(Auth0Client).to receive(:new).and_return(auth0_stub) }
     before { expect(auth0_stub).to receive(:user).with('auth0|1').and_return(user_data) }
 
-    it { expect(auth0.metadata).to eq('bibliotheca' => 'foo/bar', 'classroom' => 'foo/baz') }
-    it { expect(auth0.social_id).to eq('auth0|1') }
-    it { expect(auth0.user).to eq(user_data) }
+    context 'when no atheneum permissions' do
+      let(:user_data) { { id: 1, bibliotheca: { permissions: 'foo/bar' }, classroom: { permissions: 'foo/baz' } }.deep_stringify_keys }
+
+      it { expect(auth0.metadata).to eq('bibliotheca' => { 'permissions' => 'foo/bar' }, 'classroom' => { 'permissions' => 'foo/baz' }) }
+      it { expect(auth0.add_permission('atheneum', 'test/*')).to eq({
+                                                                  'bibliotheca' => { 'permissions' => 'foo/bar' },
+                                                                  'classroom' => { 'permissions' => 'foo/baz' },
+                                                                  'atheneum' => { 'permissions' => 'test/*'}
+                                                                  })}
+      it { expect(auth0.social_id).to eq('auth0|1') }
+      it { expect(auth0.user).to eq(user_data) }
+    end
+
+    context 'when atheneum permissions' do
+      let(:user_data) { { id: 2, atheneum: { permissions: 'foo/bar' } }.deep_stringify_keys }
+
+      it { expect(auth0.add_permission('atheneum', 'test/*')).to eq({'atheneum' => {'permissions' => 'foo/bar:test/*' } })}
+    end
+
+    context 'when none permissions' do
+      let(:user_data) { { id: 2 }.stringify_keys }
+
+      it { expect(auth0.add_permission('atheneum', 'test/*')).to eq({'atheneum' => {'permissions' => 'test/*' } })}
+    end
   end
 
 end
