e4a750ffeaeea3084f1318a517d654e4c68e22b8
Agustin Pina <aguspina87@gmail.com>
2016-12-20 16:26:32 -0300

    Add safe operations for store

--- lib/mumukit/auth/store.rb
+++ lib/mumukit/auth/store.rb
@@ -5,16 +5,31 @@ module Mumukit::Auth
       @db = Daybreak::DB.new "#{db_name}.db", default: '{}'
     end
 
+    def method_missing(name, *args, &block)
+      if name.to_s.starts_with? 'safe_'
+        action = name.to_s.split('safe_').last
+        self.try do |db|
+          value = db.send action, *args
+          db.close
+          return value
+        end
+      else
+        super
+      end
+    end
+
+    def close
+      @db.close
+    end
+
+    private
+
     def set!(key, value)
       @db.update! key.to_sym => value.to_json
     end
 
     def get(key)
       Mumukit::Auth::Permissions.load @db[key]
-    end
-
-    def close
-      @db.close
     end
   end
 end
--- spec/mumukit/store_spec.rb
+++ spec/mumukit/store_spec.rb
@@ -4,8 +4,8 @@ describe Mumukit::Auth::Store do
   let(:db) { Mumukit::Auth::Store.new('test') }
 
   describe '#set!' do
-    before { db.set! 'foo@bar.org', {teacher: 'bar/*'} }
-    it { expect(db.get('foo@bar.org').has_role? 'teacher').to be_true }
-    it { expect(db.get('foo@bar.org').has_role? 'student').to be_false }
+    before { db.safe_set! 'foo@bar.org', {teacher: 'bar/*'} }
+    it { expect(db.safe_get('foo@bar.org').has_role? 'teacher').to be_true }
+    it { expect(db.safe_get('foo@bar.org').has_role? 'student').to be_false }
   end
 end
