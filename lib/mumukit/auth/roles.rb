e4e52f488ffa97fc0977bb59aba13107cd93f5df
Federico Scarpa <fedescarpa@gmail.com>
2021-06-22 12:34:12 -0300

    Add ex_student role

--- lib/mumukit/auth/roles.rb
+++ lib/mumukit/auth/roles.rb
@@ -1,6 +1,6 @@
 module Mumukit::Auth
   module Roles
-    ROLES = [:student, :teacher, :headmaster, :writer, :editor, :janitor, :moderator, :forum_supervisor, :admin, :owner]
+    ROLES = [:ex_student, :student, :teacher, :headmaster, :writer, :editor, :janitor, :moderator, :forum_supervisor, :admin, :owner]
 
     ROLES.each do |role|
       define_method "#{role}?" do |scope = Mumukit::Auth::Slug.any|
